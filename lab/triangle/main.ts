fd294103607769a33abacbe8b11d485c408cfed1
Noah Bussinger <development@palto.ch>
2023-06-15 13:16:32 +0200

    Begin triangle

---
+++ lab/triangle/main.ts
@@ -0,0 +1,16 @@
+/*
+    Palto Studio
+    Developed by Noah Bussinger
+    2023
+*/
+
+window.addEventListener("compile", async (_event: Event): Promise<void> => {
+    //
+
+    function render(now: float): void {
+        //
+
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+});
